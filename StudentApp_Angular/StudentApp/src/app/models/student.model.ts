20dcc8a8b46ef390a9ed360b90a7919db30fd104
aditigh9 <aditimadan19@gmail.com>
2025-12-13 03:15:45 +0530

    Add files via upload

---
+++ StudentApp_Angular/StudentApp/src/app/models/student.model.ts
@@ -0,0 +1,11 @@
+export interface Student {
+  id: number;
+  name: string;
+  className: string;
+  gender: string;
+  hasHobby: boolean;
+  hobby?: string | null;
+  favoriteSubject?: string | null;
+}
+
+
